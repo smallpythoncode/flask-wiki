c623b33d27816849778556473c09c439d0b2fe86
small_python <smallpythoncode@gmail.com>
2021-08-25 20:40:57 +0200

    1. Added home and about templates to templates directory 2. Added ifnamemain to wiki.py 3. Added about route

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -2,7 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/home")
-def hello ():
-    return "Hello, World!"
+def home():
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
