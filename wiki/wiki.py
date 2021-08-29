4f51387e97dfc37cf7bf1d0104705267194cf983
small_python <smallpythoncode@gmail.com>
2021-08-29 21:57:44 +0200

    1. Added layout.html (single file for repeat layout code) 2. Modified aesthetic (<style>)

--- wiki/wiki.py
+++ wiki/wiki.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Small Python",
+        "title": "Post 00",
+        "content": "This is some content.",
+        "date_posted": "August 28, 2021",
+    },
+    {
+        "author": "Small Python",
+        "title": "Post 01",
+        "content": "This is some other content.",
+        "date_posted": "August 29, 2021",
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About Page")
 
 
 if __name__ == "__main__":
